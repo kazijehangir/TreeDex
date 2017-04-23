043f9773b020f9a8c334101a4db1119eb8b21be2
SaadAmer <saadamer1318@gmail.com>
2017-04-23 23:39:18 +0500

    changed register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -15,12 +15,40 @@ import {
 } from 'react-native';
 
 import containerStyles from '../styles/Container'
+import inputStyles from '../styles/Input'
 import textStyles from '../styles/Text'
 import ButtonCustom from '../components/ButtonCustom'
+import ButtonInverted from '../components/ButtonInverted'
 
-export default ({ onPress, goBack }) => (
+const usernameChangeHandler = ev => {
+  // console.log(ev.nativeEvent.text)
+}
+
+const emailChangeHandler = ev => {
+  // console.log(ev.nativeEvent.text)
+}
+const passwordChangeHandler = ev => {
+  // console.log(ev.nativeEvent.text)
+}
+
+export default ({ onPress, goBack, onPressRegister }) => (
  <View style={containerStyles.container}>
-   <Text style={textStyles.title}>Register as a new user</Text>
+   
+   <Text style={textStyles.subtitle} >Your Username will be used to login</Text>
+
+   <TextInput placeholder='Username'
+    style={inputStyles.emailInput}
+    onChange={usernameChangeHandler}></TextInput>
+
+   <TextInput placeholder='Email'
+    style={inputStyles.emailInput}
+    onChange={emailChangeHandler}></TextInput>
+
+   <TextInput placeholder='Password'
+    style={inputStyles.passwordInput}
+    secureTextEntry={true}
+    onChange={passwordChangeHandler}></TextInput>
+
    <ButtonCustom onPress={onPress} title='Or go to Login' />
    <ButtonCustom onPress={goBack} title='Go Back' />
  </View>
