30179d4be69d6f85ae0f8bb63c3f2d4ecc273dc6
mahnoormmalik <mahnoormmalik@gmail.com>
2017-04-24 00:42:51 +0500

    newss

--- src/pages/News.js
+++ src/pages/News.js
@@ -20,6 +20,8 @@ import textStyles from '../styles/Text'
 import ButtonCustom from '../components/ButtonCustom'
 import ButtonSquare from '../components/ButtonSquare'
 import Colors from '../Colors'
+import { WebView } from 'react-native';
+
 export default () => (
  <View style={containerStyles.container}>
    {/*<Text style={textStyles.title}>This is the News page</Text>
@@ -40,15 +42,19 @@ export default () => (
       </Card.Actions>
     </Card>*/}
     <View style = {{marginTop: 10, marginLeft: 10, marginRight: 10}}>
-        <Image source={require('../images/plant.jpg')} style={{width: Constants.width, height: 200, marginBottom: 10}}>
-            <Text style={textStyles.welcomeLabel1}>There is Such a Thing as Plant Intelligence</Text>
-        </Image>
-        <Text style = {{fontFamily:'Roboto', fontSize: 16, color: Colors.primary}}>Plants are capable of solving problems and learning from past experiences</Text>
+        <TouchableHighlight onPress={() =>this._onPressImage1}>
+            <Image source={require('../images/plant.jpg')} style={{width: Constants.width, height: 200, marginBottom: 10}}>
+                <Text style={textStyles.welcomeLabel1}>There is Such a Thing as Plant Intelligence</Text>
+            </Image>
+        </TouchableHighlight>
+            <Text style = {{fontFamily:'Roboto', fontSize: 16, color: Colors.primary}}>Plants are capable of solving problems and learning from past experiences</Text>
+        
     </View>
     
     <View style={{flex: 1, flexDirection: 'row', marginTop: 20}}>
-        <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}/>
-        <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}/>
+            <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}/>
+            <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}/>
+        
     </View>
     <View style={{flex:1, flexDirection: 'row', marginTop: 40}}>
         <View style = {{flex:0.5, marginLeft: 10}}>
@@ -60,3 +66,15 @@ export default () => (
     </View>
  </View>  
 )
+/*_onPressImage1() {
+    class MyWeb extends Component {
+        render() {
+            return (
+            <WebView
+                source={{uri: 'https://google.com'}}
+                style={{marginTop: 20}}
+            />
+            );
+        }
+    }
+}*/
