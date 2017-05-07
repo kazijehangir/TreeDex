9c83e4ca754548b4197edd6a8bd02e8c54c64dce
mahnoormmalik <mahnoormmalik@gmail.com>
2017-05-08 02:12:10 +0500

    register fix

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -20,9 +20,10 @@ import inputStyles from '../styles/Input'
 import textStyles from '../styles/Text'
 import ButtonCustom from '../components/ButtonCustom'
 import ButtonInverted from '../components/ButtonInverted'
-import * as firebase from "firebase";
+// import * as firebase from "firebase";
 import Header from '../components/header'
 import Constants from '../Constants'
+
 /*var state = {}
 
 const usernameChangeHandler = ev => {
@@ -97,6 +98,81 @@ export default ({onPress, goBack, onSuccessRegister}) => (
 )*/
 
 class Register extends React.Component{
+  constructor(props){
+    super(props);
+  //  alert(JSON.stringify(this.props))
+    this.state = {
+      loaded: false,
+      email: '',
+      password: ''
+    };
+    // alert(this.state.email)
+  }
+  signup(){
+
+    this.setState({
+      loaded: true
+    });
+    // alert(this.state.passnword)
+    Constants.firebaseApp.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(function(user){
+      this.setState({
+        email: '',
+        password: '',
+        loaded: false
+      });
+      alert('Your account was created!');
+      this.props.onSuccessRegister()
+    }).catch(function(error) {
+        switch(error.code){
+
+          case "EMAIL_TAKEN":
+            alert("The new user account cannot be created because the email is already in use.");
+          break;
+
+          case "INVALID_EMAIL":
+            alert("The specified email is not a valid email.");
+          break;
+
+          default:
+            alert("Error creating user:");
+        }
+
+      
+
+    });
+
+  }
+  render(){
+    return(
+       <View style={containerStyles.container}>
+   
+          <Text style={textStyles.subtitle} >Sign Up</Text>
+          <ActivityIndicator animating = {this.state.loaded}
+              style = {containerStyles.activityIndicator} size = "large"
+          />
+          <TextInput placeholder='Username'
+          style={inputStyles.emailInput}
+          ></TextInput>
+
+          <TextInput placeholder='Email'
+          style={inputStyles.emailInput}
+          onChangeText={(text)=> this.setState({email:text})}
+          value={this.state.email}></TextInput>
+
+          <TextInput placeholder='Password'
+          style={inputStyles.passwordInput}
+          secureTextEntry={true}
+          onChangeText={(text)=>this.setState({password: text})}
+          value={this.state.password}></TextInput>
+
+          <ButtonInverted title='Register' onPress={this.signup.bind(this)} />
+
+          <ButtonCustom onPress={this.props.onPress} title='Or go to Login' />
+
+        </View>
+    )
+  }
 
 }
-export default Register
+AppRegistry.registerComponent('signup', () => signup);
+export default Register;
