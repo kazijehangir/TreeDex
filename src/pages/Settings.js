a46e23ab68efcce0f96a5ef96af79144de89fbfc
Saad Amer <Saad Amer>
2017-05-08 02:11:49 +0500

    settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -6,72 +6,30 @@ import {
   TextInput,
   Image,
   View,
-  ListView,
   TouchableHighlight,
   NavigationExperimental,
   Dimensions,
+  ScrollView,
   Platform,
-  BackAndroid,
-  TouchableOpacity
+  BackAndroid
 } from 'react-native';
 
 import containerStyles from '../styles/Container'
 import textStyles from '../styles/Text'
 import ButtonCustom from '../components/ButtonCustom'
-import Constants from '../Constants'
-import buttonStyles from '../styles/Button.js'
+import ButtonSquare from '../components/ButtonSquare'
 
-const Row = (props) => (
-  <View style={containerStyles.friendsList}>
-    <Image source={require('../images/profilePic.png')} style={containerStyles.photo} />
-    <Text style={textStyles.friends}>
-      {props.name}
-    </Text>
-  </View>
-);
+//If you gonna change this please put it somewhere back too!!!
+// I put them back. - JK
 
-const Header = (props) => (
-  <View style={containerStyles.searchBar}>
-    <TextInput
-      style={textStyles.input}
-      placeholder="Search..."
-      onChangeText={(text) => console.log('searching for ', text)}
-    />
-  </View>
-);
+export default ({onPressQuests, onPressNews}) => (
+  <ScrollView style={containerStyles.explore}>
+    <Text style={textStyles.header}>Explore</Text>
+    <ButtonSquare onPress={onPressNews} title='Change Username'/>
+    <ButtonSquare onPress={onPressQuests} title= 'Change Password'/>
 
-const Footer = (props) => (
-  <View style={containerStyles.footer}>
-    <TouchableOpacity style={buttonStyles.footerButton} onPress={() => console.log('load more')}>
-      <Text style={textStyles.footerText}>Load More</Text>
-    </TouchableOpacity>
-  </View>
-);
 
-class Friends extends React.Component {
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows([{name:'Ali'}, {name:'Wali'}]),
-    };
-  }
-
-  render() {
-    return (
-      <View style={containerStyles.Container}>
-        <Text style={textStyles.header}>Friends</Text>
-        <ListView
-          Style={containerStyles.friendsList}
-          dataSource={this.state.dataSource}
-          renderRow={(data) => <Row {...data} />}
-          renderSeparator={(sectionId, rowId) => <View key={rowId} style={containerStyles.separator} />}
-          renderHeader={() => <Header />}
-          renderFooter={() => <Footer />}
-        />
-      </View>
-    );
-  }
-}
-
-export default Friends;
+    
+    
+  </ScrollView>
+)
