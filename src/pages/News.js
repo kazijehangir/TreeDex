4af9903c94e83697056fea1cec7e77755d62dcc2
Jehangir Kazi <kazi@jehangir.me>
2017-05-09 23:04:32 +0500

    converted news to class and set header

--- src/pages/News.js
+++ src/pages/News.js
@@ -22,47 +22,48 @@ import ButtonSquare from '../components/ButtonSquare'
 import Colors from '../Colors'
 import { WebView } from 'react-native';
 
-export default ({onPressNews, onPressSubNews1, onPressSubNews2}) => (
- <View style={containerStyles.container}>
-    <View style = {{marginTop: 10, marginLeft: 10, marginRight: 10}}>
-        <TouchableHighlight onPress={onPressNews}>
-            <Image source={require('../images/plant.jpg')} style={{width: Constants.width, height: 200, marginBottom: 10}}>
-                <Text style={textStyles.welcomeLabel1}>There is Such a Thing as Plant Intelligence</Text>
-            </Image>
-        </TouchableHighlight>
-            <Text style = {{fontFamily:'Roboto', fontSize: 16, color: Colors.primary}}>Plants are capable of solving problems and learning from past experiences</Text>
+class News extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  componentWillMount() {
+    this.props.setHeaderTitle('News')
+  }
+  render() {
+    return (
+     <View style={containerStyles.container}>
+        <View style = {{marginTop: 10, marginLeft: 10, marginRight: 10}}>
+            <TouchableHighlight onPress={this.props.onPressNews}>
+                <Image source={require('../images/plant.jpg')} style={{width: Constants.width, height: 200, marginBottom: 10}}>
+                    <Text style={textStyles.welcomeLabel1}>There is Such a Thing as Plant Intelligence</Text>
+                </Image>
+            </TouchableHighlight>
+                <Text style = {{fontFamily:'Roboto', fontSize: 16, color: Colors.primary}}>Plants are capable of solving problems and learning from past experiences</Text>
 
-    </View>
-
-    <View style={{flex: 1, flexDirection: 'row', marginTop: 20}}>
-            <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}/>
-            <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}/>
-
-        <TouchableHighlight onPress={onPressSubNews1} style={{flex:0.5}}>
-            <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}></Image>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={onPressSubNews2} style={{flex:0.5}}>
-            <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}></Image>
-        </TouchableHighlight>
-    </View>
-    <View style={{flex:1, flexDirection: 'row', marginTop: 10}}>
-        <View style = {{flex:0.5, marginLeft: 10}}>
-            <Text style={textStyles.newsHeading}>Bats Drawn to Plant via "Echo Beacon"</Text>
         </View>
-         <View style = {{flex:0.5, marginLeft: 13, marginRight:10}}>
-            <Text style={textStyles.newsHeading}>Teenager Is on Track to Plant a Trillion Trees</Text>
+
+        <View style={{flex: 1, flexDirection: 'row', marginTop: 20}}>
+                <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}/>
+                <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}/>
+
+            <TouchableHighlight onPress={this.props.onPressSubNews1} style={{flex:0.5}}>
+                <Image source={require('../images/news1.jpg')} style={{flex:0.5, width: undefined, height: 200, marginLeft: 10, marginRight: 5}}></Image>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={this.props.onPressSubNews2} style={{flex:0.5}}>
+                <Image source={require('../images/news2.jpg')} style={{flex: 0.5, width: undefined, height: 200, marginLeft: 5, marginRight: 10}}></Image>
+            </TouchableHighlight>
         </View>
-    </View>
- </View>
-)
-/*_onPressImage1() {
-    class MyWeb extends Component {
-        render() {
-            return (
-            <WebView
-                source={{uri: 'https://google.com'}}
-                style={{marginTop: 20}}
-            />
-            );
-        }
-    }*/
+        <View style={{flex:1, flexDirection: 'row', marginTop: 10}}>
+            <View style = {{flex:0.5, marginLeft: 10}}>
+                <Text style={textStyles.newsHeading}>Bats Drawn to Plant via "Echo Beacon"</Text>
+            </View>
+             <View style = {{flex:0.5, marginLeft: 13, marginRight:10}}>
+                <Text style={textStyles.newsHeading}>Teenager Is on Track to Plant a Trillion Trees</Text>
+            </View>
+        </View>
+     </View>
+    )
+  }
+}
+export default News
