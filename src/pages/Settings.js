866fcd0527bf4f9eda7138d26e04b42cebc44cf3
Jehangir Kazi <kazi@jehangir.me>
2017-05-10 00:10:37 +0500

    changed chngeEmail

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -22,17 +22,24 @@ import ButtonSquare from '../components/ButtonSquare'
 //If you gonna change this please put it somewhere back too!!!
 // I put them back. - JK
 
-export default ({onPressPass, onPressEmail}) => (
-
-  <View style={{flexDirection: 'row', width: 360, height: 50, justifyContent: 'center'}} >
-    <ScrollView style={containerStyles.explore}>
-      <Text style={textStyles.header}>Settings</Text>
-      <ButtonSquare onPress={onPressEmail} title='Change Email'/>
-      <ButtonSquare onPress={onPressPass} title= 'Change Password'/>
-
-
-
-
-    </ScrollView>
-  </View>
-)
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  componentWillMount() {
+    this.props.setHeaderTitle('Settings')
+  }
+  render() {
+    return (
+      <View style={{flexDirection: 'row', width: 360, height: 50, justifyContent: 'center'}} >
+        <ScrollView style={containerStyles.explore}>
+          <Text style={textStyles.header}>Settings</Text>
+          <ButtonSquare onPress={this.props.onPressEmail} title='Change Email'/>
+          <ButtonSquare onPress={this.props.onPressPass} title= 'Change Password'/>
+        </ScrollView>
+      </View>
+    )
+  }
+}
